20954b5b7839c915e5dbb4db4799fba64bd30795
Deepak Kumar <deepak.kumar@sysbiz.org>
2018-10-29 19:12:41 +0530

    added log4j configuration to resources and pom

--- Behavior Design Patterns/StrategyDesignPattern/src/main/java/com/deepak/gof/strategy/AnimalStrategyPattern.java
+++ Behavior Design Patterns/StrategyDesignPattern/src/main/java/com/deepak/gof/strategy/AnimalStrategyPattern.java
@@ -1,21 +1,26 @@
 package com.deepak.gof.strategy;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class AnimalStrategyPattern {
+
+	private static final Logger logger = LogManager.getLogger(AnimalStrategyPattern.class);
 	
 	public static void main(String[] args){
 		
 		Animal sparky = new Dog();
 		Animal tweety = new Bird();
 		
-		System.out.println("Dog: " + sparky.tryToFly());
+		logger.info("Dog: " + sparky.tryToFly());
 		
-		System.out.println("Bird: " + tweety.tryToFly());
+		logger.info("Bird: " + tweety.tryToFly());
 		
 		// This allows dynamic changes for flyingType
 		
 		sparky.setFlyingAbility(new ItFlys());
 		
-		System.out.println("Dog: " + sparky.tryToFly());
+		logger.info("Dog: " + sparky.tryToFly());
 		
 	}
 	
